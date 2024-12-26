96afb3198e5fa27fd1c98489c0857f2c739289dd
Vikash565-dot <vikashranjankumar565@gmail.com>
2024-12-26 10:57:45 +0530

    Update c2.cpp

--- c2.cpp
+++ c2.cpp
@@ -7,22 +7,26 @@ private:
     stack<int> mainStack; 
     stack<int> minStack;  
 public:
-    MinStack();
+    MinStack() {}  // Define the constructor
+
     void push(int val) {
         mainStack.push(val);
         if (minStack.empty() || val <= minStack.top()) {
             minStack.push(val);
         }
     }
+
     void pop() {
         if (mainStack.top() == minStack.top()) {
             minStack.pop(); 
         }
         mainStack.pop();
     }
+
     int top() {
         return mainStack.top();
     }
+
     int getMin() {
         return minStack.top();
     }
